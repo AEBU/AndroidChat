b1e447463d0aae9018c11eec519a4cda05cd86ba
Alexis Bautista <rhack_100@outlook.com>
2016-12-29 23:47:47 -0500

    37.-ContactList Presenter y Event

---
+++ app/src/main/java/uce/optativa/androidchat/contactlist/ContactListInteractorImpl.java
@@ -0,0 +1,26 @@
+package uce.optativa.androidchat.contactlist;
+
+/**
+ * Created by Alexis on 29/12/2016.
+ */
+public class ContactListInteractorImpl implements ContactListInteractor {
+    @Override
+    public void subscribe() {
+
+    }
+
+    @Override
+    public void unsubscribe() {
+
+    }
+
+    @Override
+    public void destroyListener() {
+
+    }
+
+    @Override
+    public void removeContact(String email) {
+
+    }
+}
