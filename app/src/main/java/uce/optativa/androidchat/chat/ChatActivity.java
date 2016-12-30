c4cf88110209cf8b34d65fe5fef3e11ea55ce19e
Alexis Bautista <rhack_100@outlook.com>
2016-12-30 14:24:03 -0500

    45.-Layout Chat tenemos para cada usuario el chat de ingreso

---
+++ app/src/main/java/uce/optativa/androidchat/chat/ChatActivity.java
@@ -0,0 +1,15 @@
+package uce.optativa.androidchat.chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uce.optativa.androidchat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
